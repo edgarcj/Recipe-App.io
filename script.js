56a6e3b5e37d9b024447ef4d4985a925078e9e69
Edgar Ernesto Campo Jaime <68762462+edgarcj@users.noreply.github.com>
2021-07-06 21:47:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -216,6 +216,6 @@ popupClose.addEventListener("click", () => {
 if ("servicWorker" in navigator) {
   navigator.serviceWorker
     .register("./sw.js")
-    .then((reg) => console.log("Registro de SW exitoso"))
+    .then((reg) => console.log("Registro de SW exitoso", reg))
     .catch((err) => console.warn("Error al tratar de 	registrar el sw", err));
 }
